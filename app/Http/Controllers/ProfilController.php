0d39da067985ea75136532403c4fa995c186bd13
faudincahyo <fingergreem6@gmail.com>
2023-01-11 00:04:12 +0700

    ProfilController New

--- app/Http/Controllers/ProfilController.php
+++ app/Http/Controllers/ProfilController.php
@@ -8,24 +8,37 @@ use Illuminate\Http\Request;
 class ProfilController extends Controller
 {
     public function index(){
-        return view('layouts.admin.profil');
-    }
-    public function create()
-    {
-        $profil = Profil::query()
-        ->get();
-        return view('layouts.admin.profilinput', compact('profil'));
+        $data = Profil::query()->get();
+        return view('layouts.admin.profil', compact('data'));
     }
 
-    public function store(Request $request)
+    public function edit($id)
     {
-        Profil::create([
+        $data = Profil::find($id);
+        if (empty($data)) {
+            # jika data kosong empty() maka 
+            return redirect()->back()->with('galat', 'Data Belum di Tambahkan');
+            # fungsi with() adalah untuk membawa notifikasi dengan session yang berupa pemberitahuan
+        }
+
+        return view('layouts.admin.profilinput', compact('data'));
+
+    }
+
+
+
+    public function update(Request $request, $id){
+        $data = Profil::find($id);
+
+        $data->update([
             'alamat_kel' => $request->alamat_kel,
             'telp_kel' => $request->telp_kel,
             'alamat_rt' => $request->alamat_rt,
-            'telp_rt' => $request->alamat_kel,
+            'telp_rt' => $request->telp_rt,
         ]);
 
-        return redirect()->back()->with('success', 'Data Berhasil di Perbarui');
+        return redirect()->route('profil.index')->with('berupdate', 'Data Berhasil di Perbarui');
+
     }
+
 }
